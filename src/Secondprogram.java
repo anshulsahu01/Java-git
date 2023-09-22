8ce80d877ff22862acdf144802b36dd22f2751e0
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-22 21:14:23 +0530

    try

--- src/Secondprogram.java
+++ src/Secondprogram.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Secondprogram {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
+        System.out.println("enter the age");
         int age = obj.nextInt();
-        System.out.println("enter the age"+ age);
+        System.out.println("your age is  "+age);
     }
 }
