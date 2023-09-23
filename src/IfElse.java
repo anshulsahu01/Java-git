000857ffc386e4454e5e7e53d93643d0755d001d
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-23 10:23:09 +0530

    if else

---
+++ src/IfElse.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IfElse {
+
+    public static void main(String[] args) {
+
+        Scanner obj = new Scanner(System.in);
+
+        System.out.println("enter your age");
+
+        int age = obj.nextInt();
+//        System.out .println( age);
+
+        if ( age >=18){
+            System.out.println( "you are adult");
+
+        } else{
+            System.out.println("you are miner");
+        }
+
+
+    }
+}
