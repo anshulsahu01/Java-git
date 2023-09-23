af2b154709d167b1215efafcfac847059dca155f
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-23 11:03:19 +0530

    String  if
     else
    
      code

---
+++ src/Stingifelse.java
@@ -0,0 +1,18 @@
+public class Stingifelse {
+    public static void main(String[] args) {
+
+        String s1= "anshul";
+        String s2 = "anshul";
+//        System.out.println(s1 +s2);
+
+        if( s1 == s2){
+            System.out.println("s1,s2 are same");
+
+        }
+        else{
+            System.out.println("they are diffrent");}
+    }
+
+
+
+}
